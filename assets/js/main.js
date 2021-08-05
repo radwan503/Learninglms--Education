8ca132b352a8ad78906f2bd0a9e91d51c13c2734
Radwan Ahmed Anik <radwancse503@gmail.com>
2021-08-06 00:26:23 +0600

    counter

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,18 @@ var couresBox = document.querySelectorAll('.individual-courses');
 for(i= 0; i<couresBox.length;i++){
    couresBox[i].style.backgroundColor  = coursesColor[Math.floor(Math.random() * coursesColor.length)];
 }*/
+
+
+// counter testmonial
+
+$('.counter-num').each(function () {
+   $(this).prop('Counter',0).animate({
+       Counter: $(this).text()
+   }, {
+       duration: 4000,
+       easing: 'swing',
+       step: function (now) {
+           $(this).text(Math.ceil(now));
+       }
+   });
+});
